8ab96bcdd0f0d22e0f52ef79533359454fb1f708
Carlos Guzmán <guzmanc@utb.edu.co>
2023-11-27 14:03:08 -0500

    id, modify

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import List  # Import List from typing module
+from typing import List
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -41,27 +41,30 @@ class OrderUpdate(BaseModel):
     quantity: int
     status: str
 
-class OrderResponse(OrderCreate):
+class OrderResponse(BaseModel):
     id: int
+    type: str
+    quantity: int
+    status: str
 
 @app.post("/orders/", response_model=OrderResponse)
 def create_order(order: OrderCreate):
     order_id = len(orders_db) + 1
     order_obj = Order(id=order_id, bottle_type=order.bottle_type, quantity=order.quantity, status=order.status)
     orders_db[order_id] = order_obj
-    return {"id": order_id, **order.dict()}
+    return {"id": order_id, "type": order.bottle_type, "quantity": order.quantity, "status": order.status}
 
 @app.get("/orders/{order_id}", response_model=OrderResponse)
 def read_order(order_id: int):
     order = orders_db.get(order_id)
     if order is None:
         raise HTTPException(status_code=404, detail="Order not found")
-    return {"id": order.id, "bottle_type": order.bottle_type, "quantity": order.quantity, "status": order.status}
+    return {"id": order.id, "type": order.bottle_type, "quantity": order.quantity, "status": order.status}
 
-@app.get("/orders/", response_model=List[OrderResponse])  # Use List[OrderResponse] here
+@app.get("/orders/", response_model=List[OrderResponse])
 def read_orders(skip: int = 0, limit: int = 10):
     orders = list(orders_db.values())[skip : skip + limit]
-    return [{"id": order.id, "bottle_type": order.bottle_type, "quantity": order.quantity, "status": order.status} for order in orders]
+    return [{"id": order.id, "type": order.bottle_type, "quantity": order.quantity, "status": order.status} for order in orders]
 
 @app.put("/orders/{order_id}", response_model=OrderResponse)
 def update_order(order_id: int, order_update: OrderUpdate):
@@ -73,7 +76,7 @@ def update_order(order_id: int, order_update: OrderUpdate):
     order._quantity = order_update.quantity
     order._status = order_update.status
 
-    return {"id": order.id, "bottle_type": order.bottle_type, "quantity": order.quantity, "status": order.status}
+    return {"id": order.id, "type": order.bottle_type, "quantity": order.quantity, "status": order.status}
 
 @app.delete("/orders/{order_id}", response_model=dict)
 def delete_order(order_id: int):
